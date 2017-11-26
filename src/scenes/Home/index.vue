54964c0b368a8cf5a996d7923fb680e889dc3997
niels <niels@appademic.nl>
2017-11-26 12:25:31 +0100

    Moved images to CDN

--- src/scenes/Home/index.vue
+++ src/scenes/Home/index.vue
@@ -167,27 +167,27 @@
         popularGames: [{
           title: 'Black Squad',
           slug: 'black-squad',
-          thumbnail: 'https://files.graph.cool/cj59tz3nfrlhy0105885qy550/cjagnywjp02m101568bbio7v2'
+          thumbnail: 'https://www.nadespots.com/cdn/cjagnywjp02m101568bbio7v2'
         }, {
           title: 'S.K.I.L.L.: Special Force 2',
           slug: 'skill-special-force-2',
-          thumbnail: 'https://files.graph.cool/cj59tz3nfrlhy0105885qy550/cjagny9z102lx0156ndgni4aj'
+          thumbnail: 'https://www.nadespots.com/cdn/cjagny9z102lx0156ndgni4aj'
         }, {
           title: 'Counter Strike: Global Offensive',
           slug: 'counter-strike-global-offensive',
-          thumbnail: 'https://files.graph.cool/cj59tz3nfrlhy0105885qy550/cjagnxfea02lt0156xkmwu3h6'
+          thumbnail: 'https://www.nadespots.com/cdn/cjagnxfea02lt0156xkmwu3h6'
         }, {
           title: 'Alliance of Valiant Arms',
           slug: 'alliance-of-valiant-arms',
-          thumbnail: 'https://files.graph.cool/cj59tz3nfrlhy0105885qy550/cjagnsw0002hv0183mtgpptw2'
+          thumbnail: 'https://www.nadespots.com/cdn/cjagnsw0002hv0183mtgpptw2'
         }, {
           title: 'Warface',
           slug: 'warface',
-          thumbnail: 'https://files.graph.cool/cj59tz3nfrlhy0105885qy550/cjagnwp5l02lh0156fhqmva5p'
+          thumbnail: 'https://www.nadespots.com/cdn/cjagnwp5l02lh0156fhqmva5p'
         }, {
           title: 'Line of Sight',
           slug: 'line-of-sight',
-          thumbnail: 'https://files.graph.cool/cj59tz3nfrlhy0105885qy550/cjaa2otcp03y601419mel2kvj'
+          thumbnail: 'https://www.nadespots.com/cdn/cjaa2otcp03y601419mel2kvj'
         }]
       }
     },
